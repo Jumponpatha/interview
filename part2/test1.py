18910e78b18376781a4074ef2d918258912c7b00
Jumponpatha Chaimongkonrojna <91845870+Jumponpatha@users.noreply.github.com>
2024-01-09 22:59:25 +0700

    Add files via upload

---
+++ part2/test1.py
@@ -0,0 +1,25 @@
+# Import requests library
+import requests
+
+# Using get data from API link
+x = requests.get('https://randomuser.me/api')
+
+# print(x.status_code)
+# print(x.text)
+
+# Using if/else for checking status code equal 200
+if x.status_code == 200:
+    data = x.json()
+
+    users = data['results']
+
+    # Input data from API link to result
+    for user in users:
+        first_name = user['name']['first']
+        last_name = user['name']['last']
+        gender = user['gender']
+
+    # Print the result firstname, lastname and gender
+    print(f'First Name: {first_name} Last Name: {last_name} Gender: {gender}')
+else:
+    print('ERROR API')
